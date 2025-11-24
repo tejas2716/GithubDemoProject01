ce2826922fcc4d10bde8586fbd4889eff4f9cfcb
tejas2716 <sathetejas111@gmail.com>
2025-11-24 19:58:03 +0530

    implements service method

--- GithDemoProject01/src/main/java/com/example/service/StudentServices.java
+++ GithDemoProject01/src/main/java/com/example/service/StudentServices.java
@@ -1,5 +1,19 @@
 package com.example.service;
 
-public class StudentServices {
+import com.example.entity.Student;
+
+public class StudentServices implements IStudentService {
+
+	@Override
+	public Student addStudent(Student s) {
+		
+		return null;
+	}
+
+	@Override
+	public Student addAllStudent(Student s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
