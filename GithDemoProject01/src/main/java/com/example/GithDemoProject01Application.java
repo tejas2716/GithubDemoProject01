139438d782696be432adc29819b6da977ec746b2
tejas2716 <sathetejas111@gmail.com>
2025-11-24 11:02:08 +0530

    first commit

---
+++ GithDemoProject01/src/main/java/com/example/GithDemoProject01Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithDemoProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithDemoProject01Application.class, args);
+	}
+
+}
